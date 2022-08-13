b77929da25896e7929836f2c48a94ceccfaa7a78
Neha Sen <neha712513@gmail.com>
2022-08-14 02:38:12 +0530

    ok

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,12 @@
 import { TbMessages } from 'react-icons/tb'
 import { Link } from 'react-router-dom'
-
+import {useLocation} from 'react-router-dom'
 
 export const Navbar = () => {
+
+    const location = useLocation();
+
+
     return (
         <>
 
@@ -11,9 +15,27 @@ export const Navbar = () => {
                 <div className='text-center rounded-full bg-pink-700 p-1'>
                     <TbMessages className='text-3xl text-emerald-300' /></div>
                 <div className='flex justify-evenly p-1 lg:w-2/4 md:w-2/4 w-full'>
-                    <Link to='/'><p className='hover:text-pink-600 cursor-pointer'>Contacts</p></Link>
-                    <Link to='/contact/create'><p className='hover:text-pink-600 cursor-pointer'>Create Contact</p></Link>
-                    <Link to='/message'><p className='hover:text-pink-600 cursor-pointer'>Messages</p></Link>
+                    <Link to='/'>
+                        <div className='group'>
+                        <p className={location.pathname==='/'? 'text-pink-600 cursor-pointer' :'cursor-pointer group-hover:text-pink-500'}>
+                            Contacts</p> 
+                        <div className={location.pathname==='/'? 'bg-pink-600 m-auto transition-transform group-hover:scale-x-150 group-hover:ease-linear delay-200 duration-700 h-0.5 w-6' : 'bg-white m-auto transition-transform group-hover:scale-x-150 group-hover:ease-linear delay-200 duration-700 h-0.5 w-6'}></div>
+                        </div>
+                            </Link>
+                    <Link to='/contact/create'>
+                        <div className='group'>
+                        <p className={location.pathname==='/contact/create'? 'text-pink-600 cursor-pointer' :'cursor-pointer group-hover:text-pink-500'}>
+                            Create Contact</p> 
+                        <div className={location.pathname==='/contact/create'? 'bg-pink-600 m-auto transition-transform group-hover:scale-x-150 group-hover:ease-linear delay-200 duration-700 h-0.5 w-6' : 'bg-white m-auto transition-transform group-hover:scale-x-150 group-hover:ease-linear delay-200 duration-700 h-0.5 w-6'}></div>
+                        </div>
+                    </Link>
+                    <Link to='/message'>
+                        <div className='group'>
+                        <p className={location.pathname==='/message'? 'text-pink-600 cursor-pointer' :'cursor-pointer group-hover:text-pink-500'}>
+                            Messages</p> 
+                            <div className={location.pathname==='/message'? 'bg-pink-600 m-auto transition-transform group-hover:scale-x-150 group-hover:ease-linear delay-200 duration-700 h-0.5 w-6' : 'bg-white m-auto transition-transform group-hover:scale-x-150 group-hover:ease-linear delay-200 duration-700 h-0.5 w-6'}></div>
+                        </div>
+                    </Link>
                 </div>
 
             </div>
